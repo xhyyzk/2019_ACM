bc49dffad972a26102dd769a01c4dc0b123128fa
xhyyzk <46747274+xhyyzk@users.noreply.github.com>
2019-09-12 21:01:35 +0800

    hyk

--- 图论/最短路/Dij.cpp
+++ 图论/最短路/Dij.cpp
@@ -1,51 +1,36 @@
-#include <cstdio>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
-#include <cstring>
+#include<bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+const int MAXN = 1e6 + 10;
+const ll INF = (1ll << 60);
 
-const int N = 1e6 + 5;
-const int M = 1e6 + 5;
-const int INF = 0x3f3f3f3f;
-
-int n, m;
+int n , m , s , t;
 
 struct Node {
-    int d, pos;
-    bool operator < (const Node & rhs) const {
-        return d > rhs.d;
+    intd , pos;
+    bool operator < (const Node &r) const {
+        return d > r.d;
     }
 };
 
 struct Edge {
-    int to, w;
-    int next;
-}edge[M];
+    int u , v , w;
+};
 
-int head[N], tot;
-
-void init(int n) {
-    for(int i = 0; i <= n; i++) head[i] = -1;
-    tot = 0;
-}
+int dis[MAXN];
+int vis[MAXN];
+vector<Edge>edge[MAXN];
+priority_queue<Node>q;
 
 void add(int u, int v, int w) {
-    edge[tot].to = v; edge[tot].w = w;
-    edge[tot].next = head[u];
-    head[u] = tot++;
+    edge[u].push_back(Edge{u, v, w});
+
 }
-
-int dis[N];
-int vis[N];
-
-priority_queue <Node > q;
 
 void Dijstra(int s) {
     while(!q.empty()) q.pop();
     memset(vis, 0, sizeof(vis));
-    for(int i = 1; i <= n; i++) dis[i] = INF;
+    for(int i = 0; i <= n + 1; i++) dis[i] = INF;
     dis[s] = 0;
     q.push(Node{0,s});
     while(!q.empty() ) {
@@ -55,17 +40,12 @@ void Dijstra(int s) {
         if(vis[u]) continue;
         vis[u] = 1;
         int len = edge[u].size();
-        for(int i = head[u]; i != -1; i = edge[i].next) {
-            Edge v = edge[i].to;
-            if(dis[v] > dis[u] + edge[i].w) {
-                dis[v] = dis[u] + edge[i].w;
-                q.push(Node {dis[v], v});
+        for(int i = 0; i < len; i++) {
+            Edge &e = edge[u][i];
+            if(dis[e.v] > dis[u] + e.w) {
+                dis[e.v] = dis[u] + e.w;
+                q.push(Node {dis[e.v], e.v});
             }
         }
     }
-}
-
-int main() {
-
-    return 0;
 }
